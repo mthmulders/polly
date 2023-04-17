0bc757d696bbf89dc9e4af10a42419a4136aefbb
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-04-17 21:58:15 +0200

    test: Better display names

--- test/src/test/java/it/mulders/polly/test/HomepageIT.java
+++ test/src/test/java/it/mulders/polly/test/HomepageIT.java
@@ -1,7 +1,10 @@
 package it.mulders.polly.test;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class HomepageIT extends AbstractPlaywrightTest {
     @Test
     void should_display_homepage() {
