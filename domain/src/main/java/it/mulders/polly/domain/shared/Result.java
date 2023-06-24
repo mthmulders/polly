b0b38ee31a78b511b22a182909343f8bdb1978df
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-06-24 21:02:00 +0200

    Introduce generic Result class with tests

---
+++ domain/src/main/java/it/mulders/polly/domain/shared/Result.java
@@ -0,0 +1,118 @@
+package it.mulders.polly.domain.shared;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Modelled after Scala's Try class, this is a container for either the outcome of a successful computation or a failure.
+ * @param <T> The type of the computation result.
+ */
+public sealed interface Result<T> {
+    T getValue();
+
+    <R> Result<R> map(Function<T, R> converter);
+
+    <R> Result<R> flatMap(Function<T, Result<R>> converter);
+
+    Throwable getCause();
+
+    T getOrElse(T other);
+
+    default T getOrElseGet(Supplier<T> supplier) {
+        return getOrElse(supplier.get());
+    }
+
+    T getOrElseConvertCause(Function<Throwable, T> converter);
+
+    static <T> Result<T> success() {
+        return new Success<>();
+    }
+
+    static <T> Result<T> of(T value) {
+        return new Success<>(value);
+    }
+
+    static <T> Result<T> of(Throwable cause) {
+        return new Failure<>(cause);
+    }
+
+    final class Success<T> implements Result<T> {
+        private final T value;
+
+        public Success() {
+            this.value = null;
+        }
+
+        public Success(final T value) {
+            this.value = value;
+        }
+
+        @Override
+        public T getValue() {
+            return this.value;
+        }
+
+        @Override
+        public <R> Result<R> map(Function<T, R> converter) {
+            return new Success<>(converter.apply(this.value));
+        }
+
+        @Override
+        public <R> Result<R> flatMap(Function<T, Result<R>> converter) {
+            return converter.apply(value);
+        }
+
+        @Override
+        public Throwable getCause() {
+            throw new IllegalStateException("This success value has no throwable cause");
+        }
+
+        @Override
+        public T getOrElse(T other) {
+            return this.value;
+        }
+
+        @Override
+        public T getOrElseConvertCause(Function<Throwable, T> converter) {
+            return this.value;
+        }
+    }
+
+    final class Failure<T> implements Result<T> {
+        private final Throwable cause;
+
+        public Failure(final Throwable cause) {
+            this.cause = cause;
+        }
+
+        @Override
+        public T getValue() {
+            throw new IllegalStateException(cause);
+        }
+
+        @Override
+        public <R> Result<R> map(Function<T, R> converter) {
+            return (Result<R>) this;
+        }
+
+        @Override
+        public <R> Result<R> flatMap(Function<T, Result<R>> converter) {
+            return (Result<R>) this;
+        }
+
+        @Override
+        public Throwable getCause() {
+            return cause;
+        }
+
+        @Override
+        public T getOrElse(T other) {
+            return other;
+        }
+
+        @Override
+        public T getOrElseConvertCause(Function<Throwable, T> converter) {
+            return converter.apply(cause);
+        }
+    }
+}
---
+++ domain/src/test/java/it/mulders/polly/domain/shared/ResultTest.java
@@ -0,0 +1,103 @@
+package it.mulders.polly.domain.shared;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ResultTest implements WithAssertions {
+    @Test
+    void resultSuccess_should_be_Success() {
+        assertThat(Result.success()).isInstanceOf(Result.Success.class);
+    }
+
+    @Test
+    void resultOfValue_should_be_Success() {
+        assertThat(Result.of(42)).isInstanceOf(Result.Success.class);
+    }
+
+    @Test
+    void resultOfThrowable_should_be_Failure() {
+        assertThat(Result.of(new NullPointerException())).isInstanceOf(Result.Failure.class);
+    }
+
+
+    @Nested
+    class Success {
+        private final Result.Success<Integer> success = new Result.Success<>(42);
+        @Test
+        void should_map_to_different_type() {
+            var result = success.map(String::valueOf);
+            assertThat(result.getValue()).isEqualTo("42");
+        }
+        @Test
+        void should_flatMap_to_different_type() {
+            var result = success.flatMap(x -> new Result.Success<>(String.valueOf(x)));
+            assertThat(result.getValue()).isEqualTo("42");
+        }
+
+        @Test
+        void getOrElse_should_return_value() {
+            assertThat(success.getOrElse(13)).isEqualTo(42);
+        }
+
+        @Test
+        void getOrElseGet_should_return_value() {
+            assertThat(success.getOrElseGet(() -> 13)).isEqualTo(42);
+        }
+
+        @Test
+        void should_not_have_cause() {
+            assertThatThrownBy(success::getCause).isInstanceOf(IllegalStateException.class);
+        }
+
+        @Test
+        void getOrElseConvertCause_should_return_value() {
+            assertThat(success.getOrElseConvertCause((x) -> 13)).isEqualTo(42);
+        }
+
+        @Test
+        void can_have_no_value() {
+            assertThat(new Result.Success<>().getValue()).isNull();
+        }
+    }
+
+    @Nested
+    class Failure {
+        private final Result.Failure<Integer> failure = new Result.Failure<>(new NullPointerException());
+        @Test
+        void should_not_map_to_different_type() {
+            var result = failure.map(String::valueOf);
+            assertThat(result.getCause()).isInstanceOf(NullPointerException.class);
+        }
+        @Test
+        void should_not_flatMap_to_different_type() {
+            var result = failure.flatMap(x -> new Result.Success<>(String.valueOf(x)));
+            assertThat(result.getCause()).isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        void getOrElse_should_return_other_value() {
+            assertThat(failure.getOrElse(13)).isEqualTo(13);
+        }
+
+        @Test
+        void getOrElseGet_forSuccess_should_return_value() {
+            assertThat(failure.getOrElseGet(() -> 13)).isEqualTo(13);
+        }
+
+        @Test
+        void should_have_cause() {
+            assertThat(failure.getCause()).isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        void getOrElseConvertCause_should_return_value() {
+            assertThat(failure.getOrElseConvertCause((x) -> 13)).isEqualTo(13);
+        }
+
+        @Test
+        void can_have_no_value() {
+            assertThat(new Result.Success<>().getValue()).isNull();
+        }
+    }
+}
