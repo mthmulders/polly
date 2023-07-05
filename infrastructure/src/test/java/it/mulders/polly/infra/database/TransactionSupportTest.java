e6aaa1c2c505725eb598f6017eefc18d618f5b24
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-07-05 15:51:53 +0200

    Improve test coverage of TransactionSupport class

--- infrastructure/src/test/java/it/mulders/polly/infra/database/TransactionSupportTest.java
+++ infrastructure/src/test/java/it/mulders/polly/infra/database/TransactionSupportTest.java
@@ -1,7 +1,11 @@
 package it.mulders.polly.infra.database;
 
 import it.mulders.polly.domain.shared.Result;
+import jakarta.transaction.HeuristicMixedException;
+import jakarta.transaction.HeuristicRollbackException;
+import jakarta.transaction.NotSupportedException;
 import jakarta.transaction.RollbackException;
+import jakarta.transaction.SystemException;
 import jakarta.transaction.UserTransaction;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -11,24 +15,19 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TransactionSupportTest implements WithAssertions {
     static class DummyTransaction implements UserTransaction {
-        private final boolean shouldRollback;
         private boolean isRolledBack;
         private boolean isCommitted;
 
-        public DummyTransaction(final boolean shouldRollback) {
-            this.shouldRollback = shouldRollback;
-        }
+        public DummyTransaction() {}
 
         @Override
-        public void begin() {}
+        public void begin() throws NotSupportedException, SystemException {}
 
         @Override
-        public void commit() throws RollbackException, SecurityException, IllegalStateException {
-            if (shouldRollback) {
-                throw new RollbackException();
-            } else {
-                isCommitted = true;
-            }
+        public void commit()
+                throws SystemException, RollbackException, SecurityException, IllegalStateException,
+                        HeuristicMixedException, HeuristicRollbackException {
+            isCommitted = true;
         }
 
         @Override
@@ -47,11 +46,44 @@ class TransactionSupportTest implements WithAssertions {
         @Override
         public void setTransactionTimeout(int seconds) {}
     }
-    ;
+
+    @Test
+    void failure_starting_transaction_through_SystemException_should_fail_return_failure() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void begin() throws SystemException {
+                throw new SystemException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TechnicalTransactionException.class);
+    }
+
+    @Test
+    void failure_starting_transaction_through_NotSupportedException_should_fail_return_failure() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void begin() throws NotSupportedException {
+                throw new NotSupportedException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TechnicalTransactionException.class);
+    }
 
     @Test
     void after_run_should_return_success() {
-        var support = new TransactionSupport(new DummyTransaction(false));
+        var support = new TransactionSupport(new DummyTransaction());
 
         var result = support.runTransactional(() -> {});
 
@@ -60,7 +92,7 @@ class TransactionSupportTest implements WithAssertions {
 
     @Test
     void after_run_should_commit() {
-        var transaction = new DummyTransaction(false);
+        var transaction = new DummyTransaction();
         var support = new TransactionSupport(transaction);
 
         support.runTransactional(() -> {});
@@ -71,7 +103,7 @@ class TransactionSupportTest implements WithAssertions {
     @Test
     void after_failure_should_return_Throwable() {
         var throwable = new RuntimeException("Woopsadaisy");
-        var support = new TransactionSupport(new DummyTransaction(false));
+        var support = new TransactionSupport(new DummyTransaction());
 
         var result = support.runTransactional(() -> {
             throw throwable;
@@ -85,19 +117,91 @@ class TransactionSupportTest implements WithAssertions {
 
     @Test
     void after_failure_should_rollback_transaction() {
-        var throwable = new RuntimeException("Woopsadaisy");
-        var transaction = new DummyTransaction(false);
+        var transaction = new DummyTransaction();
 
         new TransactionSupport(transaction).runTransactional(() -> {
-            throw throwable;
+            throw new RuntimeException("Woopsadaisy");
         });
 
         assertThat(transaction.isRolledBack).isTrue();
     }
 
     @Test
+    void after_SystemException_should_rollback_transaction() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void commit() throws SystemException {
+                throw new SystemException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TechnicalTransactionException.class);
+    }
+
+    @Test
+    void after_HeuristicMixedException_should_rollback_transaction() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void commit() throws HeuristicMixedException {
+                throw new HeuristicMixedException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TransactionRolledBackException.class);
+    }
+
+    @Test
+    void after_HeuristicRollbackException_should_rollback_transaction() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void commit() throws HeuristicRollbackException {
+                throw new HeuristicRollbackException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TransactionRolledBackException.class);
+    }
+
+    @Test
+    void after_SecurityException_should_rollback_transaction() {
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void commit() throws SecurityException {
+                throw new SecurityException("Woopsadaisy");
+            }
+        });
+
+        var result = support.runTransactional(() -> {});
+
+        assertThat(result)
+                .isInstanceOf(Result.Failure.class)
+                .extracting(Result::getCause)
+                .isInstanceOf(TransactionRolledBackException.class);
+    }
+
+    @Test
     void after_rollback_should_return_failure() {
-        var support = new TransactionSupport(new DummyTransaction(true));
+        var support = new TransactionSupport(new DummyTransaction() {
+            @Override
+            public void commit() throws RollbackException, SecurityException, IllegalStateException {
+                throw new RollbackException();
+            }
+        });
 
         var result = support.runTransactional(() -> {});
 
