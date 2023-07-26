f18c2835e62b46bd73c25c74d97c0c44f121004c
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-07-26 20:28:52 +0200

    test: Describe contract of Poll entity

--- domain/src/main/java/it/mulders/polly/domain/polls/Poll.java
+++ domain/src/main/java/it/mulders/polly/domain/polls/Poll.java
@@ -1,5 +1,7 @@
 package it.mulders.polly.domain.polls;
 
+import java.util.Objects;
+
 /**
  * Aggregate root for the <strong>Polls</strong> domain. A <strong>Poll</strong> is a question where participants can
  * cast their votes from a predefined set of possible answers.
@@ -9,8 +11,8 @@ public class Poll {
     private String slug;
 
     public Poll(String question, String slug) {
-        this.slug = slug;
-        this.question = question;
+        this.slug = Objects.requireNonNull(slug, "Slug must not be null");
+        this.question = Objects.requireNonNull(question, "Question must not be null");
     }
 
     public String getQuestion() {
@@ -19,5 +21,18 @@ public class Poll {
 
     public String getSlug() {
         return slug;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof Poll other)) return false;
+
+        return this.question.equals(other.question) && this.slug.equals(other.slug);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(question, slug);
     }
 }
