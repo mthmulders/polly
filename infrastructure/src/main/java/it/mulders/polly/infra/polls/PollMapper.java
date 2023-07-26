c263f8427014deee866b0e178e0d46c206bd37c4
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-07-26 22:53:29 +0200

    fix: Make sure no options doesn't break the Domain model

--- infrastructure/src/main/java/it/mulders/polly/infra/polls/PollMapper.java
+++ infrastructure/src/main/java/it/mulders/polly/infra/polls/PollMapper.java
@@ -1,10 +1,12 @@
 package it.mulders.polly.infra.polls;
 
+import static org.mapstruct.NullValueMappingStrategy.RETURN_DEFAULT;
+
 import it.mulders.polly.domain.polls.Poll;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.JAKARTA)
+@Mapper(componentModel = MappingConstants.ComponentModel.JAKARTA, nullValueIterableMappingStrategy = RETURN_DEFAULT)
 public interface PollMapper {
 
     Poll pollEntityToPoll(PollEntity entity);
