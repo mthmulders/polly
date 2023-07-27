37e57943ba2323654665e4a0c47db35f6d04b252
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-07-27 21:00:47 +0200

    fix: Prevent "duplicate column 'poll_id'" upon insert of PollOptionEntity

--- infrastructure/src/main/java/it/mulders/polly/infra/polls/PollOptionEntity.java
+++ infrastructure/src/main/java/it/mulders/polly/infra/polls/PollOptionEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
@@ -20,7 +19,6 @@ public class PollOptionEntity {
     private UUID id;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "poll_id", nullable = false)
     private PollEntity poll;
 
     @Column(name = "display_value")
