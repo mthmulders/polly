ba86e9a3be60390400dfe0f474bdfe773c16a8ea
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-08-02 20:06:19 +0200

    feat: Pass client identification when creating Ballot

--- web/src/main/java/it/mulders/polly/web/display/VoteController.java
+++ web/src/main/java/it/mulders/polly/web/display/VoteController.java
@@ -8,12 +8,13 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
-import jakarta.mvc.MvcContext;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 
 @Path("/vote")
@@ -29,11 +30,16 @@ public class VoteController {
     @Inject
     private VotingService votingService;
 
+    @Context
+    @Inject
+    private HttpServletRequest request;
+
     public VoteController() {}
 
-    VoteController(Models models, PollRepository pollRepository, VotingService votingService) {
+    VoteController(Models models, PollRepository pollRepository, HttpServletRequest request, VotingService votingService) {
         this.models = models;
         this.pollRepository = pollRepository;
+        this.request = request;
         this.votingService = votingService;
     }
 
@@ -49,8 +55,8 @@ public class VoteController {
     }
 
     private Ballot prepareBallot(final Poll poll) {
-        // TODO Must add some client identification (like the JSESSIONID)
-        return votingService.requestBallotFor(poll, "");
+        var sessionId = request.getSession(true).getId();
+        return votingService.requestBallotFor(poll, sessionId);
     }
 
     private Response populateModelAndPrepareResponse(final Ballot ballot) {
--- web/src/test/java/it/mulders/polly/web/display/VoteControllerTest.java
+++ web/src/test/java/it/mulders/polly/web/display/VoteControllerTest.java
@@ -6,9 +6,9 @@ import it.mulders.polly.domain.votes.VotingServiceImpl;
 import it.mulders.polly.web.test.InMemoryPollRepository;
 import jakarta.mvc.Models;
 import jakarta.ws.rs.core.Response;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import org.assertj.core.api.WithAssertions;
@@ -16,17 +16,19 @@ import org.eclipse.krazo.core.ModelsImpl;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class VoteControllerTest implements WithAssertions {
     private final Models models = new ModelsImpl();
     private final Poll poll = new Poll("", "whatever", Collections.emptySet());
     private final InMemoryPollRepository pollRepository = new InMemoryPollRepository(Set.of(poll));
+    private final MockHttpServletRequest httpRequest = new MockHttpServletRequest();
     private final DummmyVotingService votingService = new DummmyVotingService();
 
     @Test
     void without_matching_poll_instance_should_return_NotFound() {
-        var controller = new VoteController(models, pollRepository, votingService);
+        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
         var response = controller.displayVotePage("non-existing");
 
@@ -35,7 +37,7 @@ class VoteControllerTest implements WithAssertions {
 
     @Test
     void with_matching_poll_instance_should_return_OK() {
-        var controller = new VoteController(models, pollRepository, votingService);
+        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
         var response = controller.displayVotePage(poll.getSlug());
 
@@ -44,7 +46,7 @@ class VoteControllerTest implements WithAssertions {
 
     @Test
     void with_matching_poll_instance_should_populate_model() {
-        var controller = new VoteController(models, pollRepository, votingService);
+        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
         controller.displayVotePage(poll.getSlug());
 
@@ -54,15 +56,17 @@ class VoteControllerTest implements WithAssertions {
 
     @Test
     void with_matching_poll_instance_should_create_ballot() {
-        var controller = new VoteController(models, pollRepository, votingService);
+        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
         controller.displayVotePage(poll.getSlug());
 
-        assertThat(votingService.ballotsCreatedFor).contains(poll);
+        assertThat(votingService.createdBallots).containsKey(poll);
+        var ballot = votingService.createdBallots.get(poll);
+        assertThat(ballot.clientIdentifier()).isEqualTo(httpRequest.getSession(false).getId());
     }
 
     private static class DummmyVotingService extends VotingServiceImpl {
-        public final Collection<Poll> ballotsCreatedFor = new HashSet<>();
+        public final Map<Poll, Ballot> createdBallots = new HashMap<>();
 
         public DummmyVotingService() {
             super(null);
@@ -70,8 +74,10 @@ class VoteControllerTest implements WithAssertions {
 
         @Override
         public Ballot requestBallotFor(Poll poll, String clientIdentifier) {
-            ballotsCreatedFor.add(poll);
-            return new Ballot(poll, UUID.randomUUID().toString(), "01234567");
+            var ticketId = UUID.randomUUID().toString();
+            var ballot = new Ballot(poll, clientIdentifier, ticketId);
+            createdBallots.put(poll, ballot);
+            return ballot;
         }
     }
 }
