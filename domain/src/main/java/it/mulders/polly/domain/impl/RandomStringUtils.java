7a58972da249ddfa2b93690e8333a8c5bda83b08
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-08-03 11:54:30 +0200

    style: Ensure utility class cannot be instantiated

--- domain/src/main/java/it/mulders/polly/domain/impl/RandomStringUtils.java
+++ domain/src/main/java/it/mulders/polly/domain/impl/RandomStringUtils.java
@@ -11,6 +11,8 @@ public class RandomStringUtils {
         'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
 
+    private RandomStringUtils() {}
+
     /**
      * Generate a human-readable random string that contains only certain characters. This method skips a few
      * letters from the alphanumeric input space:
