7431cbbeb3185f01018d1e5d2c0c4db903515598
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-08-03 12:21:17 +0200

    test: Verify unhappy cases for JPA-implementation of Ballot Repository

--- infrastructure/src/test/java/it/mulders/polly/infra/votes/JpaBallotRepositoryIT.java
+++ infrastructure/src/test/java/it/mulders/polly/infra/votes/JpaBallotRepositoryIT.java
@@ -36,11 +36,22 @@ class JpaBallotRepositoryIT extends AbstractJpaRepositoryTest<BallotRepository, 
     void should_store_ballot_for_poll() {
         var clientIdentifier = UUID.randomUUID().toString();
         var poll = prepareHelperEntities("should-store-ballot-for-poll");
-        var ticketId = "01234567";
+        var ticketId = ticketId();
         var ballot = new Ballot(poll, clientIdentifier, ticketId);
         runTransactional(() -> repository.store(ballot));
 
         assertThat(repository.findByTicketId(ticketId)).hasValue(ballot);
+    }
+
+    @Test
+    void should_not_find_by_non_existing_ticketId() {
+        var clientIdentifier = UUID.randomUUID().toString();
+        var poll = prepareHelperEntities("should-not-find-by-non-existing-ticketId");
+        var ticketId = ticketId();
+        var ballot = new Ballot(poll, clientIdentifier, ticketId);
+        runTransactional(() -> repository.store(ballot));
+
+        assertThat(repository.findByTicketId(ticketId())).isEmpty();
     }
 
     @Test
@@ -53,6 +64,32 @@ class JpaBallotRepositoryIT extends AbstractJpaRepositoryTest<BallotRepository, 
 
         assertThat(repository.findByPollAndClientIdentifier(poll, clientIdentifier))
                 .hasValue(ballot);
+    }
+
+    @Test
+    void should_not_find_by_nonexistent_poll() {
+        var clientIdentifier = UUID.randomUUID().toString();
+        var ticketId = ticketId();
+        var poll = prepareHelperEntities("should-not-find-by-nonexistent-poll-1");
+        var ballot = new Ballot(poll, clientIdentifier, ticketId);
+        runTransactional(() -> repository.store(ballot));
+
+        var anotherPoll = prepareHelperEntities("should-not-find-by-nonexistent-poll-2");
+
+        assertThat(repository.findByPollAndClientIdentifier(anotherPoll, clientIdentifier))
+                .isEmpty();
+    }
+
+    @Test
+    void should_not_find_by_nonexistent_clientIdentifier() {
+        var ticketId = ticketId();
+        var poll = prepareHelperEntities("should-not-find_by-nonexistent-clientIdentifier");
+        var ballot = new Ballot(poll, UUID.randomUUID().toString(), ticketId);
+        runTransactional(() -> repository.store(ballot));
+
+        assertThat(repository.findByPollAndClientIdentifier(
+                        poll, UUID.randomUUID().toString()))
+                .isEmpty();
     }
 
     private String ticketId() {
