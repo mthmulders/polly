77f9093d92bae3fca1e9278443a6a8cb4d558510
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-08-22 21:27:36 +0200

    doc: Document the domain model

--- domain/src/main/java/it/mulders/polly/domain/votes/Ballot.java
+++ domain/src/main/java/it/mulders/polly/domain/votes/Ballot.java
@@ -3,6 +3,9 @@ package it.mulders.polly.domain.votes;
 import it.mulders.polly.domain.polls.Poll;
 import java.util.Objects;
 
+/**
+ * The <strong>Ballot</strong> entity represents the right for an individual to cast one (1) vote in a {@link Poll}.
+ */
 public record Ballot(Poll poll, String clientIdentifier, String ticketId) {
     public Ballot {
         Objects.requireNonNull(poll, "Poll must not be null");
--- domain/src/main/java/it/mulders/polly/domain/votes/Vote.java
+++ domain/src/main/java/it/mulders/polly/domain/votes/Vote.java
@@ -4,6 +4,9 @@ import it.mulders.polly.domain.polls.Option;
 import it.mulders.polly.domain.polls.Poll;
 import java.util.Objects;
 
+/**
+ * The <strong>Vote</strong> data object represents a vote cast in a {@link Poll}.
+ */
 public record Vote(Poll poll, Ballot ballot, Option option) {
     public Vote {
         Objects.requireNonNull(poll, "Poll must not be null");
