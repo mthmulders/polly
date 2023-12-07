f27c7c6905b69397bae811d2f32091ab06fd58d8
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-12-07 21:57:49 +0100

    test: Validate behaviour of Vote controller when it comes to voting

--- web/src/test/java/it/mulders/polly/web/display/VoteControllerTest.java
+++ web/src/test/java/it/mulders/polly/web/display/VoteControllerTest.java
@@ -1,7 +1,11 @@
 package it.mulders.polly.web.display;
 
+import it.mulders.polly.domain.polls.Option;
 import it.mulders.polly.domain.polls.Poll;
+import it.mulders.polly.domain.shared.Result;
 import it.mulders.polly.domain.votes.Ballot;
+import it.mulders.polly.domain.votes.NonExistingBallotException;
+import it.mulders.polly.domain.votes.Vote;
 import it.mulders.polly.domain.votes.VotingServiceImpl;
 import it.mulders.polly.web.test.InMemoryPollRepository;
 import jakarta.mvc.Models;
@@ -14,6 +18,7 @@ import org.assertj.core.api.WithAssertions;
 import org.eclipse.krazo.core.ModelsImpl;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
@@ -25,45 +30,82 @@ class VoteControllerTest implements WithAssertions {
     private final MockHttpServletRequest httpRequest = new MockHttpServletRequest();
     private final DummmyVotingService votingService = new DummmyVotingService();
 
-    @Test
-    void without_matching_poll_instance_should_return_NotFound() {
-        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+    @Nested
+    class VoteDisplay {
+        @Test
+        void without_matching_poll_instance_should_return_NotFound() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
-        var response = controller.displayVotePage("non-existing");
+            var response = controller.displayVotePage("non-existing");
 
-        assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+            assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+        }
+
+        @Test
+        void with_matching_poll_instance_should_return_OK() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+
+            var response = controller.displayVotePage(poll.getSlug());
+
+            assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        }
+
+        @Test
+        void with_matching_poll_instance_should_populate_model() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+
+            controller.displayVotePage(poll.getSlug());
+
+            assertThat(models.get("poll")).isEqualTo(poll);
+            assertThat(models.get("ballot")).isNotNull();
+        }
+
+        @Test
+        void with_matching_poll_instance_should_create_ballot() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+
+            controller.displayVotePage(poll.getSlug());
+
+            assertThat(votingService.createdBallots).containsKey(poll);
+            var ballot = votingService.createdBallots.get(poll);
+            assertThat(ballot.getClientIdentifier())
+                    .isEqualTo(httpRequest.getSession(false).getId());
+        }
     }
 
-    @Test
-    void with_matching_poll_instance_should_return_OK() {
-        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+    @Nested
+    class Voting {
+        @Test
+        void without_matching_poll_instance_should_return_NotFound() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
 
-        var response = controller.displayVotePage(poll.getSlug());
+            var response = controller.processVote("non-existing", "", 0);
 
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+            assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+        }
+
+        @Test
+        void without_existing_ballot_should_return_NotFound() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+
+            var response = controller.processVote("non-existing", "", 0);
+
+            assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+        }
+
+        @Test
+        void with_existing_ballot_should_return_Whatever() {
+            var controller = new VoteController(models, pollRepository, httpRequest, votingService);
+            var ballot = votingService.requestBallotFor(poll, "");
+
+            var response = controller.processVote(poll.getSlug(), ballot.getTicketId(), 0);
+
+            assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+            assertThat(models.get("vote")).isNotNull();
+        }
     }
 
-    @Test
-    void with_matching_poll_instance_should_populate_model() {
-        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
-
-        controller.displayVotePage(poll.getSlug());
-
-        assertThat(models.get("poll")).isEqualTo(poll);
-        assertThat(models.get("ballot")).isNotNull();
-    }
-
-    @Test
-    void with_matching_poll_instance_should_create_ballot() {
-        var controller = new VoteController(models, pollRepository, httpRequest, votingService);
-
-        controller.displayVotePage(poll.getSlug());
-
-        assertThat(votingService.createdBallots).containsKey(poll);
-        var ballot = votingService.createdBallots.get(poll);
-        assertThat(ballot.getClientIdentifier())
-                .isEqualTo(httpRequest.getSession(false).getId());
-    }
+    private static final String EXISTING_TICKET_ID = "yadaa-yadaa";
 
     private static class DummmyVotingService extends VotingServiceImpl {
         public final Map<Poll, Ballot> createdBallots = new HashMap<>();
@@ -77,6 +119,17 @@ class VoteControllerTest implements WithAssertions {
             var ballot = poll.requestBallot(clientIdentifier);
             createdBallots.put(poll, ballot);
             return ballot;
+        }
+
+        @Override
+        public Result<Vote> castVote(Poll poll, String ticketId, Integer selectedOption) {
+            var existingBallot = createdBallots.values().stream()
+                    .anyMatch(ballot -> ballot.getTicketId().equals(ticketId));
+            if (existingBallot) {
+                return Result.of(new Vote(createdBallots.get(poll), new Option(selectedOption, "")));
+            } else {
+                return Result.of(new NonExistingBallotException(poll.getSlug(), ticketId));
+            }
         }
     }
 }
