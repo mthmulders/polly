3cc66665f980a50749eabb5356df8506a9c90d5a
Maarten Mulders <m.th.mulders@xs4all.nl>
2024-09-15 15:59:39 +0200

    test: Better way to verify the CSRF protection kicked in

--- test/src/test/java/it/mulders/polly/test/PollVoteIT.java
+++ test/src/test/java/it/mulders/polly/test/PollVoteIT.java
@@ -52,15 +52,13 @@ public class PollVoteIT extends AbstractPlaywrightTest {
     void should_be_protected_against_crsf() {
         page.navigate("http://localhost:9080/app/vote/jakarta-mvc");
 
-        page.evaluate("document.getElementsByName('_csrf')[0].value = 'whatever'");
-
         page.click("//label[@for='option-1']");
+
+        page.evaluate("document.getElementsByName('_csrf')[0].value = 'whatever'");
+        page.onResponse(response -> {
+            assertThat(response.status()).isEqualTo(403);
+        });
+
         page.click("//input[@type='submit']");
-
-        page.navigate("http://localhost:9080/app/vote/jakarta-mvc");
-
-        var text = page.textContent("//main");
-
-        assertThat(text).contains("You have already voted, thanks for your participation!");
     }
 }
